b4eb033551557fbdb6455426f1a9b4996731cb1a
LifebloodAspid6480 <j.ewan.giddings@gmail.com>
2025-02-07 18:46:49 -0500

    removed somthing useless

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -61,7 +61,6 @@ public class RobotContainer {
 
         SmartDashboard.putData("Auto Chooser", autoChooser);
 
-        addPPOption("forwardBack", autoChooser);
         addPPOption("simpleTest", autoChooser);
         addPPOption("driftTest", autoChooser);
         addPPOption("moonTest", autoChooser);
