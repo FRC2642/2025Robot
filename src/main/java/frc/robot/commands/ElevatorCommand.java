f020f14164c57d6ee21b5bb7dda1f45bc909100f
Aditya Meher <superzoom15.5@gmail.com>
2025-02-18 19:40:29 -0500

    commit #2 of the day

--- src/main/java/frc/robot/commands/ElevatorCommand.java
+++ src/main/java/frc/robot/commands/ElevatorCommand.java
@@ -4,18 +4,13 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.DutyCycleEncoder;
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants;
 import frc.robot.subsystems.ElevatorSubsystem;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class ElevatorCommand extends Command {
   /** Creates a new ElevatorCommand. */
   ElevatorSubsystem elevatorSubsystem;
-  private static DutyCycleEncoder shaftEncoder = new DutyCycleEncoder(Constants.ElevatorConstants.SHAFT_ENCODER_CHANNEL);
 
   int iterator;
 
@@ -36,7 +31,7 @@ public class ElevatorCommand extends Command {
   public void execute() {
     iterator++;
 
-    if (iterator >= 10) {System.out.println(shaftEncoder.get());}
+    if (iterator >= 10) {System.out.println(elevatorSubsystem.shaftEncoder.get());}
 
     iterator = (iterator >= 10) ? 0 : iterator; // Reset iterator
   }
--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -4,10 +4,14 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class ElevatorSubsystem extends SubsystemBase {
   /** Creates a new ElevatorSubsystem. */
+  public DutyCycleEncoder shaftEncoder = new DutyCycleEncoder(Constants.ElevatorConstants.SHAFT_ENCODER_CHANNEL);
+
   public ElevatorSubsystem() {}
 
   @Override
