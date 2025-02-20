c150d130ba380b2d23c6f0eb32cc665625cad529
Aditya Meher <superzoom15.5@gmail.com>
2025-02-20 11:16:16 -0500

    New SwerveModifications Subsystem
    Move all the swerve modifications I did in RobotContainer to a new subsystem. Theoretically, this should retain effectiveness.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,12 +6,9 @@ package frc.robot;
 
 import static edu.wpi.first.units.Units.*;
 
-import java.util.ArrayList;
-
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.path.PathPlannerPath;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DriverStation;
@@ -19,13 +16,13 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.commands.ElevatorCommand;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.ElevatorSubsystem;
+import frc.robot.subsystems.SwerveModifications;
 
 public class RobotContainer {
     private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
@@ -49,15 +46,9 @@ public class RobotContainer {
     private final SendableChooser<Command> autoChooser;
     
     // Custom Swerve Modifications
-    private double rotationOffset = getRotationOffset();
-    private ArrayList<Double> prevRotationOutputs = new ArrayList<>();
-    private int rotationOutputListLimit = 30;
-    private double movementPercentModifier = 0.9;
+    private final SwerveModifications swerveModifications = new SwerveModifications(drivetrain, control);
 
     private final ElevatorSubsystem elevatorSubsystem = new ElevatorSubsystem();
-    
-    private int i = 0; // iterator for debugging
-    private boolean turnDebug = false; // Set to true to enable debugging
 
     public RobotContainer() {
         configureBindings();
@@ -84,9 +75,9 @@ public class RobotContainer {
         drivetrain.setDefaultCommand(
             // Drivetrain will execute this command periodically
             drivetrain.applyRequest(() ->
-                drive.withVelocityX(-modifyAxialInput(joystick.getLeftY(), joystick.getRightTriggerAxis(), movementPercentModifier) * MaxSpeed) // Drive forward with negative Y (forward)
-                    .withVelocityY(-modifyAxialInput(joystick.getLeftX(), joystick.getRightTriggerAxis(), movementPercentModifier) * MaxSpeed) // Drive left with negative X (left)
-                    .withRotationalRate(-recieveTurnRate() * MaxAngularRate) // Drive counterclockwise with negative X (left)
+                drive.withVelocityX(-swerveModifications.modifyAxialInput(joystick.getLeftY(), joystick.getRightTriggerAxis(), swerveModifications.movementPercentModifier) * MaxSpeed) // Drive forward with negative Y (forward)
+                    .withVelocityY(-swerveModifications.modifyAxialInput(joystick.getLeftX(), joystick.getRightTriggerAxis(), swerveModifications.movementPercentModifier) * MaxSpeed) // Drive left with negative X (left)
+                    .withRotationalRate(-swerveModifications.recieveTurnRate() * MaxAngularRate) // Drive counterclockwise with negative X (left)
             )
         );
 
@@ -132,109 +123,5 @@ public class RobotContainer {
         } catch (Exception e) {
             DriverStation.reportError("Big oops: " + e.getMessage(), e.getStackTrace());
         }
-    }
-
-    private double recieveTurnRate() {
-        if (turnDebug) { i++; } // Iterator to prevent spam-logging
-
-        // Reset rotation offset 
-        if (control.getLeftBumperButtonPressed()) { rotationOffset = getRotationOffset(); }
-
-        /* Convert controller left stick x and y to degrees (0 - 360) */
-        double angle = Math.atan2(joystick.getRightY(), joystick.getRightX()); // Get raw positions
-        angle *= 180/Math.PI; // Degrees
-        angle += 90; // Turn 90 degrees to make 0 degrees up (unit circle by default)
-        if (angle > 180) { angle -= 360; }
-        if (i >= 10) {
-          System.out.print("  " + angle); // Prints the angle to the console for debugging
-        }
-
-        double currentAngle = drivetrain.getPigeon2().getRotation2d().getDegrees();
-        currentAngle = currentAngle % 360; // Reports an insanely large value by default, so % 360 to get the actual value
-        if (currentAngle > 180) { currentAngle -= 360; } // Move values more than 180 to make the degrees scale from -180 to 180
-        currentAngle *= -1; // It's flipped.
-        currentAngle += rotationOffset; // Offset rotation
-        if (currentAngle > 180) { currentAngle -= 360; }
-        
-        if (i >= 10) {
-            System.out.println(" " + currentAngle);
-            double stateAngle = drivetrain.getState().Pose.getRotation().getDegrees();
-            System.out.println(" " + stateAngle);
-        }
-
-        if (Math.abs(angle - currentAngle) > 180) { // Optimizations
-            if (angle < 0) {
-                angle += 360;
-            } else if (angle > 0) {
-                angle -= 360;
-            }
-        }
-
-        if (Math.abs(angle - currentAngle) > 180) { System.out.println("WARNING"); }
-
-        double outputPower = (angle - currentAngle) / 60; // Modifies rotational speed; retest to find the best (60)
-        if (Math.abs(outputPower) > 1) { outputPower /= Math.abs(outputPower); } // Limit the output power to (abs) 1
-
-        prevRotationOutputs.add(outputPower); // Add new values to the bottom of the list
-        while (prevRotationOutputs.size() > rotationOutputListLimit) { prevRotationOutputs.remove(0); } // Remove extra data points from the top of the list
-        if (prevRotationOutputs.size() == rotationOutputListLimit && outputPower >= 0.9) { // Use if we have data and the outputPower is set to max or near max
-            double prevOutputAvg = 0; // Get the average power over the last 30 code runs
-            for (int j = 0; j < prevRotationOutputs.size(); j++) { prevOutputAvg += Math.abs(prevRotationOutputs.get(j)); }
-            prevOutputAvg /= prevRotationOutputs.size();
-            outputPower *= prevOutputAvg; // The average is essentially a percent since power ranges from (abs) 0 to 1 so multiplying reduces power and creates acceleration
-        }
-
-        i = (i >= 10) ? 0 : i; // Reset iterator
-
-        double joystickMag = Math.sqrt(Math.pow(joystick.getRightX(), 2) + Math.pow(joystick.getRightY(), 2)); // Joystick magnitude for deadzones on friction joysticks
-        if (joystickMag >= 0.12) {
-            return outputPower;
-        } else {
-            return 0;
-        }
-    }
-
-    private double getRotationOffset() {
-        double stateRotation = drivetrain.getState().Pose.getRotation().getDegrees();
-        double gyroRotation = drivetrain.getPigeon2().getRotation2d().getDegrees();
-        gyroRotation %= 360;
-        if (gyroRotation > 180) { gyroRotation -= 360; }
-        //gyroRotation -= 90;
-        //if (gyroRotation < -180) { gyroRotation += 360; }
-        return stateRotation - gyroRotation;
-    }
-
-    /**
-     * Modifies the axial input, taking in an additional input to modify the original. Returns the result.
-     * 
-     * @param input The original input
-     * @param modifierInput The secondary, modifying input
-     * @param modifyPercent The percent of the value of the original input to be affected by the modifierInput
-     */
-
-    private double modifyAxialInput(double input, double modifierInput, double modifyPercent) {
-        input = cutValue(input, -1, 1);
-        modifierInput = cutValue(modifierInput, 0, 1);
-        double output = input * (modifierInput * modifyPercent + (1 - modifyPercent));
-        // If the input is negative, made the modifier negative, and same for positive
-        return output;
-    }
-
-    /**
-     * Limits a value between the min and the max and returns the limited value.
-     */
-
-    private double cutValue(double value, double min, double max) {
-        if (value > max) { value = max; }
-        else if (value < min) { value = min; }
-        return value;
-    }
-
-    /**
-     * @return -1 if negative and 1 if positive
-     */
-
-    private double getSign(double value) {
-        return value / Math.abs(value);
     }
 }
---
+++ src/main/java/frc/robot/subsystems/SwerveModifications.java
@@ -0,0 +1,139 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import java.util.ArrayList;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class SwerveModifications extends SubsystemBase {
+  /** Creates a new SwerveModifications. */
+  public boolean turnDebug = false;
+  private int i = 0;
+  public double rotationOffset;
+
+  public ArrayList<Double> prevRotationOutputs = new ArrayList<>();
+  public int rotationOutputListLimit = 30;
+  public double movementPercentModifier = 0.9;
+
+  private CommandSwerveDrivetrain drivetrain;
+  XboxController control;
+
+  public SwerveModifications(CommandSwerveDrivetrain drivetrainIN, XboxController controller) {
+    this.drivetrain = drivetrainIN;
+    this.control = controller;
+    this.rotationOffset = this.getRotationOffset();
+  }
+
+  public double recieveTurnRate() {
+    if (turnDebug) { i++; } // Iterator to prevent spam-logging
+
+    // Reset rotation offset 
+    if (control.getLeftBumperButtonPressed()) { rotationOffset = getRotationOffset(); }
+
+    /* Convert controller left stick x and y to degrees (0 - 360) */
+    double angle = Math.atan2(control.getRightY(), control.getRightX()); // Get raw positions
+    angle *= 180/Math.PI; // Degrees
+    angle += 90; // Turn 90 degrees to make 0 degrees up (unit circle by default)
+    if (angle > 180) { angle -= 360; }
+    if (i >= 10) {
+      System.out.print("  " + angle); // Prints the angle to the console for debugging
+    }
+
+    double currentAngle = drivetrain.getPigeon2().getRotation2d().getDegrees();
+    currentAngle = currentAngle % 360; // Reports an insanely large value by default, so % 360 to get the actual value
+    if (currentAngle > 180) { currentAngle -= 360; } // Move values more than 180 to make the degrees scale from -180 to 180
+    currentAngle *= -1; // It's flipped.
+    currentAngle += rotationOffset; // Offset rotation
+    if (currentAngle > 180) { currentAngle -= 360; }
+    
+    if (i >= 10) {
+        System.out.println(" " + currentAngle);
+        double stateAngle = drivetrain.getState().Pose.getRotation().getDegrees();
+        System.out.println(" " + stateAngle);
+    }
+
+    if (Math.abs(angle - currentAngle) > 180) { // Optimizations
+        if (angle < 0) {
+            angle += 360;
+        } else if (angle > 0) {
+            angle -= 360;
+        }
+    }
+
+    if (Math.abs(angle - currentAngle) > 180) { System.out.println("WARNING"); }
+
+    double outputPower = (angle - currentAngle) / 60; // Modifies rotational speed; retest to find the best (60)
+    if (Math.abs(outputPower) > 1) { outputPower /= Math.abs(outputPower); } // Limit the output power to (abs) 1
+
+    prevRotationOutputs.add(outputPower); // Add new values to the bottom of the list
+    while (prevRotationOutputs.size() > rotationOutputListLimit) { prevRotationOutputs.remove(0); } // Remove extra data points from the top of the list
+    if (prevRotationOutputs.size() == rotationOutputListLimit && outputPower >= 0.9) { // Use if we have data and the outputPower is set to max or near max
+        double prevOutputAvg = 0; // Get the average power over the last 30 code runs
+        for (int j = 0; j < prevRotationOutputs.size(); j++) { prevOutputAvg += Math.abs(prevRotationOutputs.get(j)); }
+        prevOutputAvg /= prevRotationOutputs.size();
+        outputPower *= prevOutputAvg; // The average is essentially a percent since power ranges from (abs) 0 to 1 so multiplying reduces power and creates acceleration
+    }
+
+    i = (i >= 10) ? 0 : i; // Reset iterator
+
+    double joystickMag = Math.sqrt(Math.pow(control.getRightX(), 2) + Math.pow(control.getRightY(), 2)); // Joystick magnitude for deadzones on friction joysticks
+    if (joystickMag >= 0.12) {
+        return outputPower;
+    } else {
+        return 0;
+    }
+  }
+
+  public double getRotationOffset() {
+    double stateRotation = drivetrain.getState().Pose.getRotation().getDegrees();
+    double gyroRotation = drivetrain.getPigeon2().getRotation2d().getDegrees();
+    gyroRotation %= 360;
+    if (gyroRotation > 180) { gyroRotation -= 360; }
+    //gyroRotation -= 90;
+    //if (gyroRotation < -180) { gyroRotation += 360; }
+    return stateRotation - gyroRotation;
+  }
+
+  /**
+   * Modifies the axial input, taking in an additional input to modify the original. Returns the result.
+   * 
+   * @param input The original input
+   * @param modifierInput The secondary, modifying input
+   * @param modifyPercent The percent of the value of the original input to be affected by the modifierInput
+   */
+
+  public double modifyAxialInput(double input, double modifierInput, double modifyPercent) {
+    input = cutValue(input, -1, 1);
+    modifierInput = cutValue(modifierInput, 0, 1);
+    double output = input * (modifierInput * modifyPercent + (1 - modifyPercent));
+    // If the input is negative, made the modifier negative, and same for positive
+    return output;
+  }
+
+  /**
+   * Limits a value between the min and the max and returns the limited value.
+   */
+
+  public double cutValue(double value, double min, double max) {
+      if (value > max) { value = max; }
+      else if (value < min) { value = min; }
+      return value;
+  }
+
+  /**
+   * @return -1 if negative and 1 if positive
+   */
+
+  public double getSign(double value) {
+      return value / Math.abs(value);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
